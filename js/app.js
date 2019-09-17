77864b9d69e081b97dae8d3ec88c164c0d9d9e6f
Duncan07 <54419424+Duncan07@users.noreply.github.com>
2019-09-17 15:36:16 -0700

    Made fixes to final portfolio submission

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 $(document).foundation()
+
+var currentTime = new Date ();
+var currentHours = currentTime.getHours();
+var currentMinutes = currentTime.getMinutes();
+var currentSeconds = currentTime.getSeconds();
+
+function setup(){
+
+
+}
+
+function draw(){
+
+  if(currentHours>0 && currentHours<6){
+
+  }
+  if(currentHours>6 && currentHours<12){
+
+  }
+  if(currentHours>12 && currentHours<18){
+    <box1 color:#9999ff>
+  }
+  if(currentHours>18 && currentHours<24){
+
+  }
+}
